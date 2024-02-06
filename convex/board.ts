1f66c9a514ef6b05435b785d44b33531c3bd45c5
thargyi <thanthtettin@gmail.com>
2024-02-06 08:06:55 +0630

    room authentication

--- convex/board.ts
+++ convex/board.ts
@@ -1,6 +1,6 @@
 import { v } from "convex/values";
 
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 const images = [
   "/placeholders/1.svg",
@@ -157,6 +157,15 @@ export const unFavorite = mutation({
     }
 
     await ctx.db.delete(existingFavorite._id);
+
+    return board;
+  },
+});
+
+export const get = query({
+  args: { id: v.id("boards") },
+  handler: async (ctx, args) => {
+    const board = ctx.db.get(args.id);
 
     return board;
   },
